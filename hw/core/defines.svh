d5652204e95b53111a3512de0016b17a46d6ca5c
小张 <418761644@qq.com>
2023-04-05 17:06:08 +0800

    initial pcRegs

--- hw/core/defines.svh
+++ hw/core/defines.svh
@@ -3,6 +3,8 @@ package defines;
 parameter NUM_WRAPS_PER_CORE 			= 4;
 parameter NUM_CORES						= 2;
 
+parameter RESET_PC                      = 0;
+
 typedef  logic[31:0]  scalar_t;
 
 
--- hw/core/fetch.sv
+++ hw/core/fetch.sv
@@ -8,6 +8,23 @@ module fetch(
 
 
 scalar_t  pcRegs[NUM_WRAPS_PER_CORE];
-assign out = 1;
+
+
+genvar wrapId;
+generate
+    for(wrapIdx = 0; wrapIdx <= NUM_WRAPS_PER_CORE; wrapIdx++)
+	begin
+		always_ff @(posedge clk, posedge reset)
+		begin
+			if (reset)
+				pcRegs[wrapIdx] <= RESET_PC;
+			else if (slectedWrapOH[wrapIdx])
+				pcRegs[wrapIdx] <= pcRegs[wrapIdx] + 4;
+		end
+	end
+
+endgenerate 
+
+
 
 endmodule 
