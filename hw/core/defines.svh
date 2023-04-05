1054822ea4fe2f6a60566f781111e0f088595dd2
小张 <418761644@qq.com>
2023-04-05 16:41:40 +0800

    add defines.svh

---
+++ hw/core/defines.svh
@@ -0,0 +1,9 @@
+package defines;
+
+parameter NUM_WRAPS_PER_CORE 			= 4;
+parameter NUM_CORES						= 2;
+
+typedef  logic[31:0]  scalar_t;
+
+
+endpackage : defines;
--- hw/core/fetch.sv
+++ hw/core/fetch.sv
@@ -1,7 +1,13 @@
+import defines::*;
+
 module fetch(
+	input in,
+	output logic out
 
 );	
 
 
+scalar_t  pcRegs[NUM_WRAPS_PER_CORE];
+assign out = 1;
 
 endmodule 
