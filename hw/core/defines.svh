5aa617b1c3717faa37fe8725a3759137f600f33b
zhangliang1997 <418761644@qq.com>
2023-07-28 22:33:38 +0800

    inst_fetch_tag.sv

--- hw/core/defines.svh
+++ hw/core/defines.svh
@@ -1,8 +1,11 @@
 package defines;
 
-parameter NUM_WARPS_PER_SM  			= 4;
-parameter NUM_WARPS_PER_SM_WIDTH 		= $log2(NUM_WARPS_PER_SM);
+parameter NUM_THREADS_PER_SM  			= 4;
+parameter NUM_THREADS_PER_SM_WIDTH 		= $log2(NUM_THREADS_PER_SM);
 parameter NUM_SMS						= 2;
+
+typedef logic[NUM_THREADS_PER_SM - 1 : 0] 				thread_bitmap_t;
+typedef logic[NUM_THREADS_PER_SM_WIDTH - 1 : 0] 		thread_idx_t;
 
 parameter RESET_PC                      = 0;
 
@@ -30,8 +33,12 @@ parameter PAGE_INDEX_BITS 			 	 = 32 - $clog2(PAGE_SIZE);
 typedef  logic[PAGE_INDEX_BITS - 1 : 0]   page_index_t;
 
 
-typedef logic [L1I_TAG_WIDTH - 1 : 0] l1i_tag_t;
-typedef logic [L1I_SETS_WIDTH - 1 : 0] l1i_set_t;
+typedef logic [L1I_TAG_WIDTH - 1 : 0] 		l1i_tag_t;
+typedef logic [L1I_SETS_WIDTH - 1 : 0] 		l1i_set_t;
+
+typedef logic [NUM_L1I_WAYS -1 : 0] 		l1i_way_bitmap_t;
+typedef logic [L1I_WAYS_WIDTH -1 : 0] 		l1i_way_idx_t;
+
 
 typedef struct packed {
 	l1i_tag_t tag;
--- hw/core/inst_fetch_tag.sv
+++ hw/core/inst_fetch_tag.sv
@@ -4,59 +4,154 @@ module ift_fetch_tag(
 	input 									    	clk,
 	input 									    	reset,
 	input 	 										pc_selected,
-	output 	logic[L1I_TAG_WIDTH - 1 : 0]  			tags[NUM_L1I_WAYS],
-	output  logic 									way_valid[NUM_L1I_WAYS]
+
+
+	// output to ift data
+	output 	logic[L1I_TAG_WIDTH - 1 : 0]  			ift_tags[NUM_L1I_WAYS],
+	output  logic[NUM_L1I_WAYS - 1 : 0] 			ift_way_valid,
+	output  thread_idx_t 							ift_selected_thread_idx,
+	output  li1_way_mask 							ift_way_valid_mask,
+
+	// input from ift data
+	input 											ifd_upate_tag_en,
+	input 	l1i_tag_mask_t							ifd_upate_tag_way_mask,
+	input	                                        ifd_cache_miss,
+	/* input   thread_idx_t							ifd_cache_miss_thread_idx, */
+
+	//csr
+	input thread_mask_t 								threadmask_en,
+	
+	// l1_l2_interface
+	input 											l12i_fill_en,
+	input [L1I_SETS_WIDTH - 1 : 0]					l12i_fill_set,
+	output logic[NUM_L1I_WAYS - 1 : 0]				l12i_fill_way_oh,
+
+	// thread_select_stage
+	input thread_mask_t								ts_thread_fetch_en_mask, 
+	// writeback module
+	input 											write_back_en,
+	input 											write_back_pc,
+	input 											write_back_threadId,
+
+
 );	
 
-	l1i_addr_t  								pcRegs[NUM_WARPS_PER_SM];
+	l1i_addr_t  								pcRegs[NUM_THREADS_PER_SM];
 	l1i_addr_t  								pc_selected;
 
-	logic[NUM_WARPS_PER_SM - 1:0] 				selectedWarpOH;
-	logic[NUM_WARPS_PER_SM_WIDTH - 1 : 0] 		selectedWarpIdx;
-	logic[NUM_WARPS_PER_SM - 1:0]  				activeMask4warpsPerSMReg;
+	logic[NUM_THREADS_PER_SM - 1:0] 				selected_thread_oh;
+	logic[NUM_THREADS_PER_SM_WIDTH - 1 : 0] 		selected_thread_idx;
+	thread_mask_t					  				thread_active_mask;
+	thread_mask_t					  				thread_fetch_en_mask;
+
+	always_ff @(posedge clk, posedge reset)
+	begin
+		if (reset)
+			thread_active_mask <= ts_thread_fetch_en_mask;
+		else
+			thread_active_mask <= thread_fetch_en_mask;
+		
+	end
+
+	assign thread_fetch_en_mask = (thread_active_mask | l2_response_thread_wakeup_mask) & ~icache_wait_mask & ts_thread_fetch_en_mask;  
+	assign icache_wait_mask = {{NUM_THREADS_PER_SM{ifd_cache_miss}} & selected_thread_oh_latched};
+
+	rr_arbiter arbiter(clk, reset, thread_fetch_en_mask, thread_selected_oh);
+
+	oh2idx   oh2idx_thread(thread_selected_oh, thread_selected_idx);
+
+	assign pc_selected = pcRegs[thread_selected_idx];
 
 
-	rr_arbiter arbiter(clk, reset, activeMask4warpsPerSMReg, selectedWarpOH);
-
-	oh2idx   oh2idxWarp(selectedWarpOH, selectedWarpIdx);
-
-	assign pc_selected = pcRegs[selectedWarpIdx];
-
-
-	genvar warpIdx;
+	genvar thread_idx;
 	generate
-		for(warpIdx = 0; warpIdx < NUM_WARPS_PER_SM; warpIdx++)
+		for(thread_idx = 0; thread_idx < NUM_WARPS_PER_SM; thread_idx++)
 		begin
 			always_ff @(posedge clk, posedge reset)
 			begin
 				if (reset)
-					pcRegs[warpIdx] <= RESET_PC;
-				else if (selectedWarpOH[warpIdx])
-					pcRegs[warpIdx] <= pcRegs[warpIdx] + 4;
+					pc_regs[thread_idx] <= RESET_PC;
+				else if (wb_rollback_en && wb_rollback_thread_idx == thread_idx)
+					pc_regs[thread_Idx] <= wb_rollback_pc;
+				else if (ifd_cache_miss && ifd_thread_idx == thread_idx)
+					pc_regs[thread_idx] <= pc_regs[thread_idx] - 4;
+				else if (thread_selected_oh[thread_idx])
+					pc_regs[thread_idx] <= pcRegs[thread_idx] + 4;
 			end
 		end
 	endgenerate 
 
+	assign sram_tag_read_en = |thread_fetch_en_mask;
 
 	genvar way_idx;
 	generate
 	for (way_idx = 0; way_idx < NUM_L1I_WAYS; way_idx++)
-		begin
-			logic line_valid[NUM_L1I_SETS];
-			sram1w1r #(
-				.SETS_NUM(NUM_L1I_SETS),
-				.DATA_WIDTH(L1I_TAG_WIDTH)		
-			 )
-			 sram_tag(
-				 .read_en(1'b1),
-				 .read_addr(pc_selected.set_idx),
-				 .read_data(tags[way_idx]),
-				 .write_en(1'b0),
-				 .write_addr(0),
-				 .write_data(0),
-				 .*
-			);
+	begin
+		logic line_valid[NUM_L1I_SETS];
+		sram1w1r #(
+			.SETS_NUM(NUM_L1I_SETS),
+			.DATA_WIDTH(L1I_TAG_WIDTH)		
+		 )
+		 sram_tag(
+			 .read_en(sram_tag_read_en),
+			 .read_addr(pc_selected.set_idx),
+			 .read_data(sram_tag_read_data[way_idx]),
+			 .write_en(sram_tag_write_en),
+			 .write_addr(sram_tag_write_addr),
+			 .write_data(sram_tag_write_data[way_idx]),
+			 .*
+		);
 
-		end 
+		always_ff (posedge clk, posedge reset)
+		begin
+			if (reset)
+			begin
+				for (int set_idx = 0; set_idx < NUM_L1I_SETS; set_idx++)
+					line_valid[set_idx] <= 0;
+			end
+			else if(
+		end
+		
+        assign way_hit_mask[way_idx] = line_valid[pc_selected.set_idx];
+	end 
 	endgenerate
+
+
+	lru cache_lru#(
+		.NUM_SETS()
+	)(
+		.fill_en(),
+		.fill_set(),
+		.fill_way_mask(),
+		.access_en(),
+		.update_en(),
+		.access_set(),
+		.access_way_mask(),
+		.*
+	);
+
+	
+	tlb iTLB#(
+		.NUM_ENTRIES(),
+		.NUM_WAYS()
+	)(
+    .lookup_en(),
+    .update_en(),
+    .invalidate_en(),
+    .invalidate_all_en(),
+    .request_vpage_idx(),
+    .request_asid(),
+    .update_ppage_idx(),
+    .update_present(),
+    .update_exe_writable(),
+    .update_supervisor(),
+    .update_global(),
+
+    .lookup_ppage_idx(),
+    .lookup_hit(),
+    .lookup_present(),
+    .lookup_exe_writable(),
+    .lookup_supervisor(),
+
+	
 endmodule 
